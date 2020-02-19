26bd82214c01a544db1787e65dae9fb4e8bdfe53
Chase Brown <krazychase@gmail.com>
2020-02-18 19:29:23 -0700

    update ls

--- unixSystems/ls.py
+++ unixSystems/ls.py
@@ -11,7 +11,7 @@ def ls():
         size = os.stat(entry).st_size
         print('{0:20} {1}B'.format(entry.name, str(size)))
 
-def lsr():
+def lsReverse():
     entries = []
     for entry in os.scandir(path):
         entries.insert(0, entry)
@@ -19,9 +19,18 @@ def lsr():
         size = os.stat(entry).st_size
         print('{0:20} {1}B'.format(entry.name, str(size)))
 
+def lsRecursive(path):
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            newPath = os.path.join(path, entry)
+            lsRecursive(newPath)
+        print('{0}'.format(entry.name))
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == '-r':
-        lsr()
+    if len(sys.argv) > 1 and sys.argv[1] == '-R':
+        lsReverse()
+    elif len(sys.argv) > 1 and sys.argv[1] == '-r':
+        lsRecursive(path)
     else:
         ls()
         
