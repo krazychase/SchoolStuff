673a56dab852ee11bcd9a52708fdf4f88d9b39d2
Chase Brown <krazychase@gmail.com>
2020-02-26 10:31:41 -0700

    update copy

---
+++ Networking/copy.py
@@ -0,0 +1,22 @@
+# Chase Brown
+# Copy a file 
+
+import sys
+
+def copy(src, dest):
+    with open(src,'rb') as source, open(dest,'wb') as destination:
+        done = False
+        while not done:
+            buf = source.read(1024)     # Read a kB at a time for speed
+            if buf: 
+                destination.write(buf)
+            else:
+                done = True
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        copy(sys.argv[1], sys.argv[2])
+    else:   
+        src = input('Source file: ')
+        dest = input('Destination file: ')
+        copy(src, dest)
