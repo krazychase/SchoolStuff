879e2dbab69f9af184a6d31f1a5043972cf00b75
Chase Brown <krazychase@gmail.com>
2020-03-03 18:19:13 -0700

    update fizzbuzz

--- cyberSecurity/fizzBuzz.c
+++ cyberSecurity/fizzBuzz.c
@@ -3,8 +3,50 @@
 
 #include <stdio.h>
 
-int main()
+void fizzBuzz(x ,y, n)
 {
-    printf("Hello");
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+		if (i % x == 0) 
+        {
+			if (i % y == 0) 
+            {
+				printf("FizzBuzz\n");
+			} 
+            else 
+            {
+				printf("Fizz\n");
+			}
+		} 
+        else if (i % y == 0) 
+        {
+			printf("Buzz\n");
+		} 
+        else 
+        {
+			printf("%d\n", i);
+        }
+    }
+}
+
+void testFizzBuzz()
+{
+    
+}
+
+int main(int argc, char** argv)
+{
+    if(argc < 1 && argv[1] == "test")
+    {
+        testFizzBuzz();
+    }
+    else
+    {
+        int x, y, n;
+        scanf("%d %d %d", &x, &y, &n);
+        fizzBuzz(x, y, n);
+    }
+    system("pause");
     return 0;
 }
