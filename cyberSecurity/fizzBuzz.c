3d02a8bfbee1b981a4e6d74d706ee59d4e11566b
Chase Brown <krazychase@gmail.com>
2020-03-06 12:18:07 -0700

    update fizzbuzz

--- cyberSecurity/fizzBuzz.c
+++ cyberSecurity/fizzBuzz.c
@@ -2,42 +2,52 @@
 // FizzBuzz
 
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-void fizzBuzz(x ,y, n)
+char* fizzBuzz(int x ,int y, int n)
 {
     int i;
+    static char answer[1000];
+    memset(answer, 0, 1000);
     for (i = 1; i <= n; i++)
     {
 		if (i % x == 0) 
         {
 			if (i % y == 0) 
             {
-				printf("FizzBuzz\n");
+				strcat(answer, "FizzBuzz\n");
 			} 
             else 
             {
-				printf("Fizz\n");
+				strcat(answer, "Fizz\n");
 			}
 		} 
         else if (i % y == 0) 
         {
-			printf("Buzz\n");
+			strcat(answer, "Buzz\n");
 		} 
         else 
         {
-			printf("%d\n", i);
+			char c[10];
+            sprintf(c, "%d\n", i);    // turn int to char
+            strcat(answer, c);
         }
     }
+    return answer;
 }
 
 void testFizzBuzz()
 {
-    
+    assert(strcmp(fizzBuzz(1, 1, 1), "FizzBuzz\n") == 0);
+    assert(strcmp(fizzBuzz(2, 4, 8), "1\nFizz\n3\nFizzBuzz\n5\nFizz\n7\nFizzBuzz\n") == 0);
+    assert(strcmp(fizzBuzz(15, 20, 10), "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n") == 0);
+    printf("Tests passed");
 }
 
 int main(int argc, char** argv)
 {
-    if(argc < 1 && argv[1] == "test")
+    if(argc > 1 && strcmp(argv[1], "test") == 0)
     {
         testFizzBuzz();
     }
@@ -45,8 +55,7 @@ int main(int argc, char** argv)
     {
         int x, y, n;
         scanf("%d %d %d", &x, &y, &n);
-        fizzBuzz(x, y, n);
+        printf("%s", fizzBuzz(x, y, n));
     }
-    system("pause");
     return 0;
 }
