466ab4ca43f8148c86e6132a8890d1a840a935a2
Chase Brown <krazychase@gmail.com>
2020-03-06 13:44:55 -0700

    update sum kind of problem

--- cyberSecurity/sumKindOfProblem.c
+++ cyberSecurity/sumKindOfProblem.c
@@ -2,8 +2,29 @@
 // Sum Kind Of Problem
 
 #include <stdio.h>
+#include <string.h>
 
-int main()
+char* skop()
 {
+    static char answer[1000];
+    strcat(answer, "skop");
+    return answer;
+}
+
+void testSkop()
+{
+    printf("Tests passed");
+}
+
+int main(int argc, char** argv)
+{
+    if(argc > 1 && strcmp(argv[1], "test") == 0)
+    {
+        testSkop();
+    }
+    else
+    {
+        printf(skop());
+    }
     return 0;
 }
