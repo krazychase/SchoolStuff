10c6780c038f2efdae10e84ec83a4866cdf3b3bd
Chase Brown <krazychase@gmail.com>
2020-03-24 18:59:36 -0600

    update midi

--- Networking/MidiToWorld/midiToWorld.py
+++ Networking/MidiToWorld/midiToWorld.py
@@ -1,7 +1,11 @@
 # Midi To World
 
 import mido
+import serial
+
+robot = serial.Serial()
 
 with mido.open_input() as inport:
     for msg in inport:
         print(msg.bytes())
+        robot.write(msg.bytes)
--- Networking/MidiToWorld/midino/midino.ino
+++ Networking/MidiToWorld/midino/midino.ino
@@ -1,9 +1,14 @@
 void setup() 
 {
-
+    Serial.begin(9600);
+    while(!Serial)
+        ;
 }
 
 void loop() 
 {
-
+    if(Serial.available() > 0)
+    {
+        message = Serial.read();
+    }
 }
