c2f5786c14931d59597291a5fc3e3567532c12aa
Chase Brown <krazychase@gmail.com>
2020-04-23 13:03:51 -0600

    update midiToWorld

--- Networking/MidiToWorld/midiToWorld.py
+++ Networking/MidiToWorld/midiToWorld.py
@@ -2,10 +2,16 @@
 
 import mido
 import serial
+import time
 
-robot = serial.Serial()
+robot = serial.Serial('COM3')
 
 with mido.open_input() as inport:
     for msg in inport:
-        print(msg.bytes())
-        robot.write(msg.bytes)
+        print(msg)
+        value = str(msg.bytes()[0]) + '|'
+        note = str(msg.bytes()[1]) + '|'
+        velocity = str(msg.bytes()[2]) + '|'
+        robot.write(value.encode())
+        robot.write(note.encode())
+        robot.write(velocity.encode())
--- Networking/MidiToWorld/midino/midino.ino
+++ Networking/MidiToWorld/midino/midino.ino
@@ -1,14 +1,50 @@
+// Midino
+
+#include<Servo.h>
+
+int servoPin2 = 2;
+int servoPin3 = 3;
+int servoPin4 = 4;
+
+Servo upDown;
+Servo leftRight;
+Servo inOut;
+
 void setup() 
 {
     Serial.begin(9600);
     while(!Serial)
         ;
+    upDown.attach(servoPin2);
+    leftRight.attach(servoPin3);
+    inOut.attach(servoPin4);
+    upDown.write(75);
+    leftRight.write(45);
+    inOut.write(75);
 }
 
 void loop() 
 {
-    if(Serial.available() > 0)
+    while(Serial.available() > 0)
     {
-        message = Serial.read();
+        int value = Serial.readStringUntil('|').toInt();
+        int note = Serial.readStringUntil('|').toInt();
+        int velocity = Serial.readStringUntil('|').toInt();
+
+        if(note == 74)
+        {
+            velocity = (velocity * 1.023) + 20;
+            upDown.write(velocity);
+        }
+        if(note == 71)
+        {
+            velocity == velocity * 1.416;
+            leftRight.write(velocity);
+        }
+        if(note == 91)
+        {
+            velocity = (velocity * 1.023);
+            inOut.write(velocity);
+        }
     }
 }
